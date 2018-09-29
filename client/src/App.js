f4a63cf0ff95284b24ecc9904ea2a470ebc33c1d
DaniAkash <s.daniakash@gmail.com>
2018-09-29 11:37:25 +0530

    install react strap

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
+import {Route, withRouter} from 'react-router-dom';
 import routes from './routes';
 import Home from './pages/Home/Home';
+import PropTypes from 'prop-types';
 
 class App extends Component {
+
+  static propTypes = {
+    history: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
+    match: PropTypes.object.isRequired,
+  }
+
+  componentDidMount() {
+    if(this.props.location.pathname === '/') {
+      this.props.history.replace(routes.home);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <Route 
+          exact
           path={routes.home} 
           component={Home} 
         />
@@ -16,4 +31,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
