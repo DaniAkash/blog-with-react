cb2e47234375ed5885d3d0f800185e1d17cb4057
DaniAkash <s.daniakash@gmail.com>
2019-02-08 13:13:52 +0530

    connect home with post data and actions

--- mobile/src/Screens/Home/Home.js
+++ mobile/src/Screens/Home/Home.js
@@ -3,7 +3,16 @@ import {
     Text
 } from 'react-native';
 
+import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as postActions from '../../redux/actions/postActions';
+
 class Home extends Component {
+
+    componentDidMount() {
+        this.props.postActions.getAllPosts();
+    }
+
     render() {
         return (
             <Text>Home</Text>
@@ -11,4 +20,18 @@ class Home extends Component {
     }
 }
 
-export default Home;
+const mapStateToProps = state => {
+    return {
+        posts: state.posts
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        postActions: bindActionCreators(postActions, dispatch),
+    };
+};
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+export default connector(Home);
