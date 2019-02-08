06735cff7f5dce6091a621e7e9bc82688e03e440
DaniAkash <dani@pickyourtrail.com>
2019-02-08 12:06:24 +0530

    create basic store

---
+++ mobile/src/redux/reducers/postsReducer.js
@@ -0,0 +1,7 @@
+import initialState from '../store/initialState';
+
+const postsReducer = (state = initialState.posts) => {
+    return state;
+}
+
+export default postsReducer;
---
+++ mobile/src/redux/store/initialState.js
@@ -0,0 +1,7 @@
+const initialState = {
+    posts: [
+
+    ],
+};
+
+export default initialState;
