37c57fe041fd099f808f8c8d66d071c4a968625b
DaniAkash <s.daniakash@gmail.com>
2019-02-08 15:15:47 +0530

    create apicall function

---
+++ mobile/src/utils/apiCall.js
@@ -0,0 +1,34 @@
+const apiCall = (route, method="GET", body={}) => {
+    return new Promise((resolve, reject) => {
+
+        const headers = new Headers({
+            'Content-Type': 'application/json',
+        });
+
+        const requestDetails = {
+            method,
+            headers,
+        };
+
+        if(method !== 'GET') {
+            requestDetails.body = JSON.stringify(body);
+        }
+
+        const serverUrl = "http://127.0.0.1:10010";
+
+        const handleErrors = response => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
+        };
+
+        fetch(`${serverUrl}/${route}`, requestDetails)
+            .then(handleErrors)
+            .then(data => resolve(data))
+            .catch(err => reject(err));
+    });
+};
+
+export default apiCall;
