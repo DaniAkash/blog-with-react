714091247b7e39513f43cce22a47146e466adc82
DaniAkash <s.daniakash@gmail.com>
2019-02-11 16:46:30 +0530

    add location options

--- mobile/src/Screens/Home/Home.js
+++ mobile/src/Screens/Home/Home.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {
     FlatList,
+    PermissionsAndroid,
+    Platform
 } from 'react-native';
 
 import {connect} from 'react-redux';
@@ -9,13 +11,50 @@ import * as postActions from '../../redux/actions/postActions';
 import Post from '../../CommonComponents/Post';
 import navigationOptions from '../../CommonComponents/CommonHeader';
 
+async function requestLocationPermission(success, failure) {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'We need device location!',
+          message: "we are using location for an experiment",
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          // use the location
+          success();
+      } else {
+        // cannot use the location
+        failure();
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+
 class Home extends Component {
 
     static navigationOptions = ({navigation}) => navigationOptions({navigation, title: "Home"});
 
     componentDidMount() {
         this.props.postActions.getAllPosts();
+        // if(Platform.OS === "android") {
+        //     requestLocationPermission(this.getLocation, () => {
+        //         alert('unable to get location');
+        //     });
+        // } else {
+        //     navigator.geolocation.requestAuthorization(); // ask permission in iOS
+        // }
     }
+
+    getLocation = () => {
+        navigator.geolocation.getCurrentPosition((data) => {
+            alert(data);
+        });
+    };
 
     _keyExtractor = (item, index) => item.id;
 
